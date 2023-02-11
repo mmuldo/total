59ce0b4afe542bb18d9fcc3846ab3e61b3a61c41
Matt Muldowney <matt.muldowney@gmail.com>
2023-02-11 12:36:24 -0800

    better main loop (no watchdog)

--- conductivity.c
+++ conductivity.c
@@ -260,27 +260,19 @@ int main(void) {
     // set system clock
     set_sys_clock_khz(CLK_KHZ, true); 
 
-    // launch core 1 code
-    multicore_reset_core1();
-    multicore_launch_core1(core1_entry);
+    while(true) {
+        // get sine frequency from serial input
+        uint32_t sine_frequency = read_int_from_serial();
 
-    // get sine frequency from serial input
-    uint32_t sine_frequency = read_int_from_serial();
-    // send frequency to core 1
-    multicore_fifo_push_blocking(sine_frequency);
+        // restart core 1 send frequency to core 1
+        multicore_reset_core1();
+        multicore_launch_core1(core1_entry);
+        multicore_fifo_push_blocking(sine_frequency);
 
-    // wait for signals to achieve steady state before taking readings
-    sleep_ms(SIGNAL_STEADY_WAIT_TIME_MS);
+        // wait for signals to achieve steady state before taking readings
+        sleep_ms(SIGNAL_STEADY_WAIT_TIME_MS);
 
-    init_adc_and_dma(sine_frequency);
-    sample_signals();
-
-    multicore_reset_core1();
-
-    // reboot system
-    watchdog_enable(
-        WATCHDOG_SYSTEM_REBOOT_WAIT_TIME_MS,
-        true
-    );
-    while(true);
+        init_adc_and_dma(sine_frequency);
+        sample_signals();
+    }
 }
