2dd2ce4d08ecfbbb2f212d0d3a513ae8fdca7162
Matt Muldowney <matt.muldowney@gmail.com>
2023-02-22 03:39:37 -0800

    modularize adc+dma stuff

--- conductivity.c
+++ conductivity.c
@@ -12,44 +12,9 @@
 #include "hardware/dma.h"
 #include "hardware/watchdog.h"
 
+// libraries in this project
 #include "i2c.h"
-
-// pins to switch between for adc round robin sampling
-#define ADC_PIN_MASK 0b0011
-
-// starting pin for adc round robin sampling
-#define ADC_FIRST_PIN 1
-
-// second pin for adc round robin sampling
-#define ADC_SECOND_PIN 0
-
-// the first gpio pin where the adc pins are located
-#define ADC_GPIO_PINS 26
-
-// adc voltage at max reading
-#define ADC_VREF 3.3
-
-// max adc reading
-#define ADC_RANGE (1 << 8)
-
-// multiply to convert adc reading to voltage
-#define ADC_CONVERT (ADC_VREF / (ADC_RANGE - 1))
-
-// number of samples in one period of a sine wave
-#define NUM_SAMPLES_PER_PERIOD 25
-
-// number of periods to sample for
-#define NUM_PERIODS 10
-
-// total number of samples that will be collected
-// NOTE: it appears that the maximum number of samples that can
-// be fed to the serial i/o at one time is 594, so if you don't
-// plan on reading the samples from serial i/o in chunks, then
-// don't let this constant exceed 594
-#define TOTAL_NUM_SAMPLES 2*NUM_PERIODS*(NUM_SAMPLES_PER_PERIOD)
-
-// frequency at which dedicated adc clock is running at
-#define ADC_CLOCK_FREQUENCY_HZ 48000000
+#include "adc.h"
 
 // pin at which sine wave will be emitted
 #define INPUT_SIGNAL_PIN 0
@@ -88,9 +53,6 @@
 
 // buffer in which to collect samples
 uint8_t samples[TOTAL_NUM_SAMPLES];
-
-// the dma channel we will use
-int dma_channel = 0;
 
 /* 
  * sine wave sample table
@@ -182,74 +144,6 @@ void core1_entry() {
     }
 }
 
-/*
- * initializes the adc pins and the dma
- *
- * Parameters
- * ----------
- *  sine_frequency : uint32_t
- *      frequency of sine waves that are being sampled;
- *      determines the sampling frequency
- */
-void init_adc_and_dma(uint32_t sine_frequency) {
-    // ADC STUFF
-    adc_gpio_init(ADC_GPIO_PINS + ADC_FIRST_PIN);
-    adc_gpio_init(ADC_GPIO_PINS + ADC_SECOND_PIN);
-    adc_init();
-    adc_set_round_robin(ADC_PIN_MASK);
-    adc_select_input(ADC_FIRST_PIN);
-
-    adc_fifo_setup(
-        true,   // write adc readings to FIFO
-        true,   // enable dma data request (DREQ)
-        1,      // DREQ asserted when at least 1 sample is present in the FIFO
-        false,  // disable inclusion of error bit
-        true    // FIFO shifts are one byte in size
-    );
-
-    // sample frequency must be twice as fast because we're sampling 2 signals
-    float sample_frequency = 2*NUM_SAMPLES_PER_PERIOD * sine_frequency;
-    adc_set_clkdiv(ADC_CLOCK_FREQUENCY_HZ / sample_frequency);
-
-
-    // DMA STUFF
-    dma_channel_config channel = dma_channel_get_default_config(dma_channel);
-
-    channel_config_set_transfer_data_size(&channel, DMA_SIZE_8);
-    channel_config_set_read_increment(&channel, false);
-    channel_config_set_write_increment(&channel, true);
-    channel_config_set_dreq(&channel, DREQ_ADC);
-
-    dma_channel_configure(
-        dma_channel,
-        &channel,
-        samples,            // write address
-        &adc_hw->fifo,      // read address
-        TOTAL_NUM_SAMPLES,  // number of transfers to do
-        false               // don't start immediately
-    );
-}
-
-/*
- * samples the sine waves using the adc
- */
-void sample_signals() {
-    dma_channel_start(dma_channel);
-    adc_run(true);
-    dma_channel_wait_for_finish_blocking(dma_channel);
-
-    // stop adc and drain fifo, just so everything is clean for next run
-    adc_run(false);
-    adc_fifo_drain();
-
-    // send samples over serial i/o
-    for (uint i = 0; i < TOTAL_NUM_SAMPLES; i++) {
-        printf("%.3f\n", (float) ADC_CONVERT*samples[i]);
-        // data tends to corrupt if we send to serial i/o too fast,
-        // so this is just to make sure it all gets printed cleanly
-        sleep_ms(1);
-    }
-}
 
 int main(void) {
     stdio_init_all();
@@ -276,8 +170,8 @@ int main(void) {
         // wait for signals to achieve steady state before taking readings
         sleep_ms(SIGNAL_STEADY_WAIT_TIME_MS);
 
-        init_adc_and_dma(sine_frequency);
-        sample_signals();
+        init_adc_and_dma(sine_frequency, samples);
+        sample_signals(samples);
         printf("%.3f\n", temperature);
         // data tends to corrupt if we send to serial i/o too fast,
         // so this is just to make sure it all gets printed cleanly
