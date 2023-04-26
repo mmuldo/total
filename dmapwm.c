89274e7ac830f691ba695b54a1005c5da0132281
Matt Muldowney <matt.muldowney@gmail.com>
2023-04-26 16:49:52 -0700

    docs

--- dmapwm.c
+++ dmapwm.c
@@ -15,12 +15,21 @@
 #define PI 3.1415926535
 #define VDD 3.3
 
+// global pointer to sine_table (needs to be global so that dma channels have access)
 uint32_t * sine_table;
+// sine table length also global just for consistency
 int sine_table_length;
 
+
+/// @brief creates an table whose entries are the pwm counter compare level corresponding to the appropriate sine function value
+/// @param length the length of the table
+/// @param amplitude desired amplitude of the sine wave
+/// @return the sine table
 uint32_t * generate_sine_table(int length, double amplitude) {
     uint32_t * sine_table = malloc(length*sizeof(uint32_t));
+
     for (int i = 0; i < length; i++) {
+        // "duty cycle" is the same thing as the pwm counter compare level in this case
         uint32_t duty_cycle = (uint32_t) round(length*((2*amplitude/VDD)*sin(2 * PI * i / length) + 1));
         sine_table[i] = duty_cycle;
     }
@@ -28,10 +37,20 @@ uint32_t * generate_sine_table(int length, double amplitude) {
     return sine_table;
 }
 
+/// @brief calculates the length of the sine table that produces a sine wave of the given frequency when the pwm clkdiv is set to 1.0
+/// @param frequency the sine wave frequency
+/// @return sine table length
 int highest_frequency_to_table_length(float frequency) {
+    // the following comes from the fact that
+    //      f_sine = f_clk / (wrap * length * clkdiv)
+    // where we assume wrap = 2*length and clkdiv = 1
     return (int) round(sqrt(CLK_KHZ * KHZ_TO_HZ / (2 * frequency)));
 }
 
+/// @brief configures the given gpio pin for pwm
+/// @param pin the pin to emit a pwm wave
+/// @param sine_table_length length of the sine table that will be fed to the pwm counter compare level (used for calculating the wrap)
+/// @return the pwm pin slice
 int configure_gpio_pwm_pin(int pin, int sine_table_length) {
     set_sys_clock_khz(CLK_KHZ, true);
 
@@ -40,8 +59,8 @@ int configure_gpio_pwm_pin(int pin, int sine_table_length) {
     
     int pwm_pin_slice = pwm_gpio_to_slice_num(INPUT_SIGNAL_PIN);
 
-    // initialize pwm config: clock division and wrap
     pwm_config config = pwm_get_default_config();
+    // it is convenient to set wrap = 2*length
     int wrap = 2*sine_table_length;
     pwm_config_set_clkdiv(&config, 1.0); 
     pwm_config_set_wrap(&config, wrap); 
@@ -50,41 +69,67 @@ int configure_gpio_pwm_pin(int pin, int sine_table_length) {
     return pwm_pin_slice;
 }
 
-void configure_and_start_pwm_dma_channels(int pwm_dma_channel, int reset_dma_channel, int pwm_pin_slice, uint32_t * sine_table, int sine_table_length) {
+/// @brief configures dma channels for setting the counter compare register of a pwm gpio pin from a table, then starts the channels
+/// @param pwm_dma_channel dma channel responsible for reading the given table and writing to the pwm counter compare register
+/// @param reset_dma_channel dma channel responsible for resetting the pwm dma channel back to the start of the table when it is done reading the table
+/// @param pwm_pin_slice pin slice of gpio pwm pin
+/// @param table a table of pwm counter compare levels to read from
+/// @param table_length the length of the table
+void configure_and_start_pwm_dma_channels(int pwm_dma_channel, int reset_dma_channel, int pwm_pin_slice, uint32_t * table, int table_length) {
     dma_channel_config pwm_dma_channel_config = dma_channel_get_default_config(pwm_dma_channel);
     dma_channel_config reset_dma_channel_config = dma_channel_get_default_config(reset_dma_channel);
 
     channel_config_set_transfer_data_size(&pwm_dma_channel_config, DMA_SIZE_32);
+    // reading entries from a table
     channel_config_set_read_increment(&pwm_dma_channel_config, true);
+    // always writes to the same place
     channel_config_set_write_increment(&pwm_dma_channel_config, false);
+    // pace with the pwm signal
     channel_config_set_dreq(&pwm_dma_channel_config, DREQ_PWM_WRAP0 + pwm_pin_slice);
+    // when done, start the reset dma channel
     channel_config_set_chain_to(&pwm_dma_channel_config, reset_dma_channel);
 
     channel_config_set_transfer_data_size(&reset_dma_channel_config, DMA_SIZE_32);
+    // only performing one read
     channel_config_set_read_increment(&reset_dma_channel_config, false);
+    // only performing one write
     channel_config_set_write_increment(&reset_dma_channel_config, false);
+    // when done, start the pwm dma channel
     channel_config_set_chain_to(&reset_dma_channel_config, pwm_dma_channel);
 
     dma_channel_configure(
         pwm_dma_channel,
         &pwm_dma_channel_config,
-        &pwm_hw->slice[pwm_pin_slice].cc,            // write address
-        sine_table,      // read address
-        sine_table_length,  // number of transfers to do
-        false               // don't start immediately
+        // write address: pwm counter compare register
+        &pwm_hw->slice[pwm_pin_slice].cc,
+        // read address: the provided table
+        table,
+        // number of transfers: the length of the table
+        table_length,
+        // don't start immediately
+        false
     );
     dma_channel_configure(
         reset_dma_channel,
         &reset_dma_channel_config,
-        &dma_hw->ch[pwm_dma_channel].read_addr,            // write address
-        &sine_table,      // read address
-        1,  // number of transfers to do
-        false               // don't start immediately
+        // write address: the pwm dma channel's read address
+        &dma_hw->ch[pwm_dma_channel].read_addr,
+        // read address: the pointer to the pointer to the first entry in the given table
+        &table,
+        // number of transfers: one (just resetting the pwm dma channel)
+        1,
+        // don't start immediately
+        false
     );
 
+    // start the channels
     dma_start_channel_mask(1u << pwm_dma_channel);
 }
 
+
+/// @brief emit a sinusoidal wave via pwm on the specified gpio pin
+/// @param gpio_pin the pin to emit the wave
+/// @param frequency the frequency of the wave
 void generate_sine_wave(int gpio_pin, float frequency) {
     sine_table_length = highest_frequency_to_table_length(frequency);
     sine_table = generate_sine_table(sine_table_length, 1);
