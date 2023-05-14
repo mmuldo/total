a9c05a44be29c2dc4e873c8e39f0de7a1cba1ae6
Matt Muldowney <matt.muldowney@gmail.com>
2023-05-13 18:59:07 -0700

    frequency sweep

--- sensor.c
+++ sensor.c
@@ -48,6 +48,14 @@
 
 #define SERIAL_IO_WAIT_TIME_US 5*60*1000000
 
+#define NUM_FREQUENCIES_FOR_SPECTROSCOPY 10
+
+#define RF 10000.0
+
+double frequencies_for_spectroscopy[] = {100, 300, 500, 700, 900, 1000, 3000, 5000, 7000, 9000};
+double magnitude_spectrum[10];
+double phase_spectrum[10];
+
 // global pointer to sine_table (needs to be global so that dma channels have access)
 uint32_t * sine_table;
 uint32_t ** sine_table_pointer;
@@ -128,7 +136,6 @@ void measure_vin_vout_initial(
     sample_signals(adc_dma_channel);
 
     average_period(samples, input_period, output_period, NUM_SAMPLES_PER_PERIOD, NUM_PERIODS);
-    printf(periods_string);
 }
 
 void measure_vin_vout_subsequent(
@@ -172,60 +179,106 @@ void measure_vin_vout_subsequent(
     sample_signals(adc_dma_channel);
 
     average_period(samples, input_period, output_period, NUM_SAMPLES_PER_PERIOD, NUM_PERIODS);
+}
+
+void print_temperature_and_pressure() {
+    double temperature, pressure;
+
+    get_temperature_and_pressure(&temperature, &pressure);
+    printf("%.3f,", temperature);
+    printf("%.3f", pressure);
+}
+
+void print_vin_vout_single_frequency(
+    double frequency,
+    double amplitude,
+    int pwm_pin,
+    int pwm_dma_channel,
+    int reset_dma_channel,
+    int adc_dma_channel
+) {
+    measure_vin_vout_subsequent(frequency, amplitude, pwm_pin, pwm_dma_channel, reset_dma_channel, adc_dma_channel);
+
+    // copy samples to buffer
+    for (int i = 0; i < NUM_SAMPLES_PER_PERIOD; i++) {
+        periods_string[i] = input_period[i];
+        periods_string[NUM_SAMPLES_PER_PERIOD + i] = output_period[i];
+    }
+
+    // ensure no zero samples (this would prematurely end the string)
+    for (int i = 0; i < 2*NUM_SAMPLES_PER_PERIOD; i++) {
+        periods_string[i] = periods_string[i] == '\0' ? 1 : periods_string[i];
+    }
+    
+    // terminate string
+    periods_string[2*NUM_SAMPLES_PER_PERIOD] = '\0';
+
     printf(periods_string);
-        
+}
+
+void print_impedence_spectrum(
+    double frequencies[],
+    int num_frequencies,
+    double amplitude,
+    int pwm_pin,
+    int pwm_dma_channel,
+    int reset_dma_channel,
+    int adc_dma_channel
+) {
+    sine vin_parameters;
+    sine vout_parameters;
+    complex Z;
+
+    for (int i = 0; i < num_frequencies; i++) {
+        measure_vin_vout_subsequent(frequencies[i], amplitude, pwm_pin, pwm_dma_channel, reset_dma_channel, adc_dma_channel);
+
+        samples_to_voltages(input_period, vin, NUM_SAMPLES_PER_PERIOD);
+        samples_to_voltages(output_period, vout, NUM_SAMPLES_PER_PERIOD);
+
+        vin_parameters = characterize(vin, frequencies[i], NUM_SAMPLES_PER_PERIOD);
+        vout_parameters = characterize(vout, frequencies[i], NUM_SAMPLES_PER_PERIOD);
+
+        Z = impedence(vin_parameters, vout_parameters, RF);
+        magnitude_spectrum[i] = Z.magnitude;
+        phase_spectrum[i] = Z.phase;
+
+    }
+
+    // print stuff
+    for (int i = 0; i < num_frequencies; i++) {
+        printf("%.3f,", magnitude_spectrum[i]);
+    }
+    for (int i = 0; i < num_frequencies-1; i++) {
+        printf("%.3f,", phase_spectrum[i]);
+    }
+    // don't include comma in last one
+    printf("%.3f", phase_spectrum[num_frequencies-1]);
 }
 
 int main(void) {
     stdio_init_all();
-    init_i2c();
-    double temperature, pressure;
-
     set_sys_clock_khz(CLK_KHZ, true);
-
-    // read in initial sine frequency
-    //float sine_frequency = read_frequency_from_serial();
-    float sine_frequency = 1000.0;
+    init_i2c();
 
     // get some dma channels we can use
     int pwm_dma_channel = dma_claim_unused_channel(true);
     int reset_dma_channel = dma_claim_unused_channel(true);
     int adc_dma_channel = dma_claim_unused_channel(true);
 
-    sine s;
+    // do a dummy run, just to get things initialized
+    float sine_frequency = 1000.0;
     measure_vin_vout_initial(sine_frequency, AMPLITUDE, INPUT_SIGNAL_PIN, pwm_dma_channel, reset_dma_channel, adc_dma_channel);
-    samples_to_voltages(input_period, vin, NUM_SAMPLES_PER_PERIOD);
-    s = characterize(vin, sine_frequency, NUM_SAMPLES_PER_PERIOD);
-
-    // for (int i = 0; i < TOTAL_NUM_SAMPLES; i += 2) {
-    //     printf("%d,", samples[i]);
-    // }
-    //print_samples(samples, samples_string);
-    if (true) {
-        get_temperature_and_pressure(&temperature, &pressure);
-        printf("%.3f\n", temperature);
-        sleep_ms(1);
-        printf("%.3f\n", pressure);
-    }
 
     while (true) {
-        // read in new sine frequency
-        //sine_frequency = read_frequency_from_serial();
-        sine_frequency = sine_frequency + 100;
-
-        measure_vin_vout_subsequent(sine_frequency, AMPLITUDE, INPUT_SIGNAL_PIN, pwm_dma_channel, reset_dma_channel, adc_dma_channel);
-        samples_to_voltages(input_period, vin, NUM_SAMPLES_PER_PERIOD);
-        s = characterize(vin, sine_frequency, NUM_SAMPLES_PER_PERIOD);
-
-        // for (int i = 0; i < TOTAL_NUM_SAMPLES; i += 2) {
-        //     printf("%d,", samples[i]);
-        // }
-        //print_samples(samples, samples_string);
-        if (true) {
-            get_temperature_and_pressure(&temperature, &pressure);
-            printf("%.3f\n", temperature);
-            sleep_ms(1);
-            printf("%.3f\n", pressure);
+        if (false) {
+            print_temperature_and_pressure();
+        } else if (true) {
+            print_impedence_spectrum(frequencies_for_spectroscopy, NUM_FREQUENCIES_FOR_SPECTROSCOPY, AMPLITUDE, INPUT_SIGNAL_PIN, pwm_dma_channel, reset_dma_channel, adc_dma_channel);
+        } else {
+            // read in new sine frequency
+            //sine_frequency = read_frequency_from_serial();
+            sine_frequency = sine_frequency + 100;
+            print_vin_vout_single_frequency(sine_frequency, AMPLITUDE, INPUT_SIGNAL_PIN, pwm_dma_channel, reset_dma_channel, adc_dma_channel);
         }
     }
 }
